958fcf644b019f6325ed68bf6d0d891b9ced747e
nickk2305 <tyunickadm@gmail.com>
2020-04-01 09:12:17 +0300

    deploy

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -9,7 +9,7 @@ const Dummy = () => {
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component
+          You are welcome!
         </div>
       </div>
     </div>
